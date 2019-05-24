cb2073c24e95c69e55b49d64877c6f8674bde02d
Vladimir Shakhov <shakhov_vs@bw-sw.com>
2019-05-24 09:16:16 +0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,46 @@
+import poplib
+import pexpect
+import time
+import sys
+import json
+from email.parser import Parser
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+server = config.get('server')
+vpnuser = config.get('vpnuser')
+vpnpassword = config.get('vpnpassword')
+email = config.get('email')
+emailpassword = config.get('emailpassword')
+popserver = config.get('popserver')
+port = config.get('port')
+
+child = pexpect.spawn('./forticlientsslvpn_cli --server {} --vpnuser {}'.format(server, vpnuser))
+child.expect('Password for VPN:')
+child.sendline(vpnpassword)
+child.expect('Would you like to connect to this server')
+child.sendline('Y')
+child.expect('An email message containing a Token Code')
+
+print 'Login successful. Waiting for email...'
+time.sleep(10)
+
+M = poplib.POP3_SSL(popserver)
+M.user(email)
+M.pass_(emailpassword)
+lines = M.retr(len(M.list()[1]))[1] # last message
+msg_content = b'\r\n'.join(lines).decode('utf-8')
+msg = Parser().parsestr(msg_content)
+code = msg.get_payload(decode=True)[34:40] # extract 6 digit code
+
+child.sendline(code)
+child.expect('STATUS::Login succeed')
+child.logfile = sys.stdout
+child.expect('STATUS::Tunnel running')
+
+route_add = 'sudo route add -net 172.16.4.0 netmask 255.255.254.0 dev ppp0'
+print route_add
+pexpect.run(route_add)
+
+child.interact()
